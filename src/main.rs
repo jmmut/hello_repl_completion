c481deba2e82746d262be12f5ecea87a4d357066
jose miguel mut <jomutlo@gmail.com>
2023-06-06 19:39:57 +0100

    hello rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+
+type AnyError = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), AnyError>{
+    let mut rl = rustyline::DefaultEditor::new()?;
+    let readline = rl.readline(">> ");
+    match readline {
+        Ok(line) => println!("Line: {:?}", line),
+        Err(_) => println!("No input"),
+    }
+    Ok(())
 }
