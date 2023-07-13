c8aa37a2d2f42401d3e9553a2506cdc1e58fe745
jose miguel mut <jomutlo@gmail.com>
2023-07-13 01:58:45 +0100

    soo cool

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,113 @@
 
 
-use reedline::{default_emacs_keybindings, ColumnarMenu, DefaultCompleter, Emacs, KeyCode, KeyModifiers, Reedline, ReedlineEvent, ReedlineMenu, Signal, DefaultPrompt};
+use reedline::{default_emacs_keybindings, ColumnarMenu, DefaultCompleter, Emacs, KeyCode, KeyModifiers, Reedline, ReedlineEvent, ReedlineMenu, Signal, DefaultPrompt, Completer, Suggestion, Span, StyledText};
+use nu_ansi_term::{Color, Style};
+
+struct IngestionCompleter;
+
+impl Completer for IngestionCompleter {
+    fn complete(&mut self, line: &str, pos: usize) -> Vec<Suggestion> {
+        let line_before_cursor = &line[0..pos];
+        let words = get_words(line_before_cursor);
+        let candidates = if words.len() == 1 {
+            complete_buckets(words.last().unwrap())
+        } else if words.len() == 2 {
+            complete_date(words.last().unwrap())
+        } else {
+            complete_stations(words.last().unwrap())
+        };
+        return candidates_to_suggestions(candidates, words.last().unwrap(), pos)
+    }
+}
+
+fn get_words(line_before_cursor: &str) -> Vec<String> {
+    let mut words = line_before_cursor.split_ascii_whitespace().collect::<Vec<_>>();
+    if let Some(char_before_cursor) = line_before_cursor.chars().last() {
+        if char_before_cursor == ' ' {
+            words.push("");
+        }
+    }
+    if words.is_empty() {
+        words.push("");
+    }
+    words.into_iter().map(|s|s.to_string()).collect()
+}
+
+fn find_substr_filter(word: &str, candidates: Vec<&str>) -> Vec<String> {
+    candidates.into_iter().filter(|candidate| candidate.to_lowercase().contains(&word.to_lowercase())).map(|s|s.to_string()).collect()
+}
+
+fn complete_buckets(word: &str) -> Vec<String> {
+    find_substr_filter(word, vec!["am-ingestion-intrasonics-rajar", "am-ingestion-tveyes", "am-ingestion-intrasonics-http-radio"])
+
+}
+fn complete_date(word: &str) -> Vec<String> {
+    find_substr_filter(word, vec!["2023-07-13"])
+}
+fn complete_stations(word: &str) -> Vec<String> {
+    find_substr_filter(word, vec!["BBC1", "BBC2", "BBC3", "radio-mansfield", "radio-hits-fm", "radio-kiss-fm",
+        "BBC-CAMBRIDGE", "BCC-LONDON", "radio-heart-east", "radio-heart-west", "radio-heart-north",
+    "very-long-station-because-it-was-renamed-at-some-point"])
+}
+
+fn candidates_to_suggestions(candidates: Vec<String>, word: &str, caret_pos: usize) -> Vec<Suggestion> {
+    candidates.into_iter().map(|candidate| {
+        Suggestion {
+            value: candidate,
+            description: None,
+            extra: None,
+            span: Span { start: caret_pos-word.len(), end: caret_pos },
+            append_whitespace: true,
+        }
+    }).collect()
+
+}
+
+#[cfg(test)]
+mod tests {
+    use reedline::Span;
+    use super::*;
+
+    #[test]
+    fn test_candidates_to_suggestions() {
+        let candidates = vec!["asdf".to_string()];
+        let caret_pos = 2;
+        let suggestions = candidates_to_suggestions(candidates,  "df", caret_pos);
+        assert_eq!(suggestions, vec![Suggestion {
+            value: "asdf".to_string(),
+            description: None,
+            extra: None,
+            span: Span { start: 0, end: caret_pos },
+            append_whitespace: true,
+        }])
+    }
+    
+    #[test]
+    fn test_get_words_empty() {
+        assert_eq!(get_words(""), vec![""]);
+    }
+    #[test]
+    fn test_get_words_first_word() {
+        assert_eq!(get_words("word"), vec!["word"]);
+    }
+    #[test]
+    fn test_get_words_second_word_empty() {
+        assert_eq!(get_words("word "), vec!["word", ""]);
+    }
+    #[test]
+    fn test_get_words_second_word() {
+        assert_eq!(get_words("word another"), vec!["word", "another"]);
+    }
+}
 
 fn main() {
-    let mut commands = vec![
-        "test".into(),
-        "hello world".into(),
-        "hello world reedline".into(),
-        "this is the reedline crate".into(),
-    ];
-    for i in 0..100 {
-        commands.push(format!("station{i}"));
-    }
-    let completer = Box::new(DefaultCompleter::new_with_wordlen(commands.clone(), 2));
-// Use the interactive menu to select options from the completer
-    let completion_menu = Box::new(ColumnarMenu::default().with_name("completion_menu"));
-// Set up the required keybindings
+    let completer = Box::new(IngestionCompleter);
+    // Use the interactive menu to select options from the completer
+    let completion_menu = Box::new(ColumnarMenu::default().with_name("completion_menu")
+        .with_text_style(Style::new().fg(Color::LightGreen).on(Color::Black))
+        .with_selected_text_style(Style::new().fg(Color::Black).on(Color::LightGreen))
+        );
+    // Set up the required keybindings
     let mut keybindings = default_emacs_keybindings();
     keybindings.add_binding(
         KeyModifiers::NONE,
